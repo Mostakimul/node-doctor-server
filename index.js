705e1ee28e73a01864df5d885e16d20694fd697c
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-11-05 12:29:01 +0100

    testing database

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
 });
 
+// Run Function
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    console.log('Databse Connected');
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
   res.send('Hello Doctor Server!');
 });
